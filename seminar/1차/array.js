9666c05d6e56c5c7e8828ad621ecdd5e7bafabf3
윤가영 <rkdkdudjd@gmail.com>
2020-10-11 19:48:46 +0900

    complete exercise

--- seminar/1차/array.js
+++ seminar/1차/array.js
@@ -26,3 +26,73 @@ console.log('popped element:'+arr.pop());
 console.log('arr pop:', arr);
 
 //2-3 shift, unshift
+arr.unshift('first item');
+console.log('arr unshift:', arr);
+arr.shift();
+console.log('arr shift:', arr);
+
+// 2-4 includes
+console.log('arr.includes(4):', arr.includes(4));
+console.log('arr.includes(1000):', arr.includes(1000));
+
+// 2-5 indexOf
+console.log('arr.indexOf(4):', arr.indexOf(4));
+
+// 2-6 concat
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var concatArr = arr1.concat(arr2);
+console.log('arr1.concat(arr2):', concatArr);
+
+// 2-7 join
+var location = ["서울", "대전", "대구", "부산"];
+console.log('location.join(\'-> \'):',location.join('-> '));
+
+//2-8 reverse
+console.log(location.reverse().join('-> '));
+
+//2-9 sort
+var countries = ['Österreich', 'Andorra', 'Vietnam'];
+console.log(countries.sort( (a,b) => a > b ? 1 : -1 ));
+console.log(countries.sort( function(a, b) {return a.localeCompare(b); }));
+console.log('오름차순 정렬:', concatArr.sort((a, b) => a - b));
+console.log('내림차순 정렬:', concatArr.sort(function(a, b) { return b - a; }));
+
+// 2-10 filter
+var number = [100, 234, -244, 255, -3000, 1103, 2000];
+var minusNumber = number.filter( item => item < 0);
+console.log('minusNumber:', minusNumber);
+
+
+// 2-11 map: 배열 요소 전체를 매개변수로 새로운 배열을 반환
+var countries = ['Österreich', 'Andorra', 'Vietnam'];
+var countriesLengths = countries.map( item => item.length );
+console.log('countriesLengths:', countriesLengths);
+
+// 2-12 reduce: 배열을 매개 변수로 받고 엘리먼트 하나를 반환할 때 하용
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var sum = number.reduce( (previousValue, currentValue) => {
+  console.log(`previousValue: ${previousValue}, currentValue: ${currentValue}`);
+  return previousValue + currentValue;
+});
+console.log('sum =', sum);
+
+//3. 배열 순회
+var serverPart = ['윤가영', '최예진', '류세화', '이수진', '석영현'];
+let serverIndexStr = '서버파트 여러분 번호 한 번 세겠습니다. "';
+
+for(let item in serverPart){
+  serverIndexStr += item + '! ';
+}
+console.log(serverIndexStr);
+
+let serverPartMemberNameStr = '서버파트 여러분 이름 한 번씩만 불러주세요~"';
+
+for(let item of serverPart) {
+  serverPartMemberNameStr += item + '! '; 
+}
+console.log(serverPartMemberNameStr);
+
+serverPart.forEach ( item => {
+  console.log(item);
+})
